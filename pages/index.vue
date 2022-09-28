6f53ff1a30b44bb7232035c9b12c8fb8f3f036bd
akikom <akikom220711@gmail.com>
2022-09-28 13:42:47 +0900

    first-commit

---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="container"> 
+    <p v-if="$auth.loggedIn">Name:{{ $auth.user.name }}</p>
+  </div>
+</template>
---
+++ pages/register.vue
@@ -0,0 +1,36 @@
+<template>
+  <form @submit.prevent="register">
+    <input type="text" v-model="name" placeholder="name" required>
+    <input type="email" v-model="email" placeholder="email" required>
+    <input type="password" v-model="password" placeholder="password" required>
+    <button type="submit">送信</button>
+  </form>
+</template>
+
+<script>
+export default {
+  auth: false,
+  data() {
+    return {
+      name: null,
+      email: null,
+      password: null
+    };
+  },
+  methods: {
+    async register() {
+      try {
+        await this.$axios.post("http://localhost:8000/api/auth/register", {
+          name: this.name,
+          email: this.email,
+          password: this.password
+        });
+        console.log("ok");
+        this.$router.push("/login");
+      } catch {
+        alert("メールアドレスが既に登録されています");
+      }
+    }
+  }
+};
+</script>
